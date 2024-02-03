86de49b2e6d48224e83e60e82c26b42fc77b0439
mcnentom <tommymcnen016@gmail.com>
2024-02-03 16:37:40 +0300

    my commit

--- src/Components/ItemDetail.jsx
+++ src/Components/ItemDetail.jsx
@@ -15,9 +15,9 @@ const ItemDetail = () => {
 
   return (
     <div id={item.id} key={item.id}>
-      <h2>{item.name}</h2>
+      <h2>Shopping items</h2>
       <img src={item.images[0]} alt="item-image" />
-      <p>{item.price} - {item.description}</p>
+      <p>{item.price}</p>
       <p>{item.title}</p>
     </div>
   );
